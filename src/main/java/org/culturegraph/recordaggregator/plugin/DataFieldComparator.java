db5c870e88e59b685cabbff9235d1373a2cf4779
eberhardtj <30794167+eberhardtj@users.noreply.github.com>
2018-12-03 10:23:39 +0100

    Refactoring
    * Compare all subfields in sorted order, when tag and ind's matching
    * Return result of comparison instead of -1, when mismatching

--- src/main/java/org/culturegraph/recordaggregator/plugin/DataFieldComparator.java
+++ src/main/java/org/culturegraph/recordaggregator/plugin/DataFieldComparator.java
@@ -14,20 +14,46 @@ public class DataFieldComparator implements Comparator<DataField> {
         this.ignoreSubfieldCode = ignoreSubfieldCode;
     }
 
+    private Comparator<Subfield> createSubfieldComparator() {
+        return new Comparator<Subfield>() {
+            @Override
+            public int compare(Subfield sf1, Subfield sf2) {
+                int codeComparison = Integer.compare(sf1.getCode(), sf2.getCode());
+                if (codeComparison != 0) return codeComparison;
+                return sf1.getData().compareTo(sf2.getData());
+            }
+        };
+    }
+
     @Override
     public int compare(DataField o1, DataField o2) {
-        if (o1.getTag().compareTo(o2.getTag()) != 0) return -1;
-        if (o1.getIndicator1() != o2.getIndicator1() || o1.getIndicator2() != o2.getIndicator2()) return -1;
+        int tagComparison = o1.getTag().compareTo(o2.getTag());
+        if (tagComparison != 0) return tagComparison;
 
-        Iterator<Subfield> iter1 = o1.getSubfields().stream().filter(sf -> sf.getCode() != ignoreSubfieldCode).iterator();
-        Iterator<Subfield> iter2 = o2.getSubfields().stream().filter(sf -> sf.getCode() != ignoreSubfieldCode).iterator();
+        int ind1Comparison = Integer.compare(o1.getIndicator1(), o2.getIndicator1());
+        if (ind1Comparison != 0) return ind1Comparison;
 
+        int ind2Comparison = Integer.compare(o1.getIndicator2(), o2.getIndicator2());
+        if (ind2Comparison != 0) return ind2Comparison;
+
+        Iterator<Subfield> iter1 = o1.getSubfields().stream()
+                .filter(sf -> sf.getCode() != ignoreSubfieldCode)
+                .sorted(createSubfieldComparator())
+                .iterator();
+
+        Iterator<Subfield> iter2 = o2.getSubfields().stream()
+                .filter(sf -> sf.getCode() != ignoreSubfieldCode)
+                .sorted(createSubfieldComparator())
+                .iterator();
+
+        Comparator<Subfield> subfieldComparator = createSubfieldComparator();
         while(iter1.hasNext() && iter2.hasNext()) {
             Subfield sf1 = iter1.next();
             Subfield sf2 = iter2.next();
 
-            if (sf1.getCode() != sf2.getCode() || !sf1.getData().equals(sf2.getData())) {
-                return -1;
+            int subfieldComparison = subfieldComparator.compare(sf1, sf2);
+            if (subfieldComparison != 0) {
+                return subfieldComparison;
             }
         }
 
